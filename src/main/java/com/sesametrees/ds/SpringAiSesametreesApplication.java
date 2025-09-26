3d1047600133c965cc239964445510d4faa8def6
Booker <wurocpengpeng@gmail.com>
2025-09-26 19:56:03 +0800

    Initial commit

---
+++ src/main/java/com/sesametrees/ds/SpringAiSesametreesApplication.java
@@ -0,0 +1,13 @@
+package com.sesametrees.ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiSesametreesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiSesametreesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sesametrees/ds/SpringAiSesametreesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesametrees.ds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiSesametreesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
